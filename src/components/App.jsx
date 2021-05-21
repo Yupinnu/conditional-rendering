ae46b73b77ca3650ff6673f25bf798afefe5a71f
Yupinnu <83113114+Yupinnu@users.noreply.github.com>
2021-05-21 23:50:27 +0700

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import Login from "./Login";
+
+var isLoggedIn = false;
+
+// const currentTime = new Date(2021, 6, 22, 21).getHours();
+// console.log(currentTime);
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+      {isLoggedIn ? <h1>Hello</h1> : <Login />}
+      {/* {currentTime > 9 && <h1>Why are you still working?</h1>} */}
     </div>
   );
 }
